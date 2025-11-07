e8544e0471c13daf431f7b7e86007413acf9c6d5
Ayush Kumar <ayushkumar16999@gmail.com>
2025-11-07 14:58:48 +0530

    Comment out VideoClips component in Home: temporarily disable the VideoClips section while maintaining the overall layout structure.

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
         <Services />
         {/* <WhyChooseMe /> */}
         {/* <Areas /> */}
-        <VideoClips />
+        {/* <VideoClips /> */}
         <CTA />
       </main>
       <Footer />
